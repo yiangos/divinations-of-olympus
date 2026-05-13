1805bc035f6e76a1f07c4ef58e60bbbdd611f9b5
Yiannis Vavouranakis <ivavouranakis@gmail.com>
2026-05-13 23:40:37 +0300

    content start

--- src/views/InfoView.vue
+++ src/views/InfoView.vue
@@ -4,16 +4,20 @@ const emit = defineEmits(['back']);
 </script>
 
 <template>
-  <div class="menu-container">
+  <div class="menu-container info-page">
     <div class="background-overlay"></div>
-    <div class="content-layer">
-      <h1>{{ title }}</h1>
+    <div class="content-layer fixed-layout">
+      <header class="sticky-header">
+        <h1>{{ title }}</h1>
+      </header>
+      
       <div class="scroll-area" v-html="content"></div>
-      <div class="footer-actions">
+      
+      <footer class="sticky-footer">
         <button class="btn-primary" @click="emit('back')">Επιστροφη</button>
-      </div>
+      </footer>
     </div>
   </div>
 </template>
-
 <style lang="scss" src="@/assets/scss/MenuView.scss" scoped></style>
+<style lang="scss" src="@/assets/scss/InfoView.scss" scoped></style>
